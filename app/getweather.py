9fc48e3ab61dd7e0decad40edfbe6519d7a02124
msolcansk <solcansky.martin@gmail.com>
2022-03-23 17:27:54 +0100

    add check for env vars in getweather

--- app/getweather.py
+++ app/getweather.py
@@ -6,6 +6,15 @@ owm_api_key = os.getenv('OWM_API_KEY')
 owm_city = os.environ.get('OWM_CITY')
 
 
+
+# Stop if environment variables are defined
+if owm_api_key is None:
+    print("OWM_API_KEY needs to be exported !")
+    exit()
+if owm_city is None:
+    print("OWM_CITY needs to be exported !")
+    exit()
+
 owm = OWM(owm_api_key)
 mgr = owm.weather_manager()
 observation = mgr.weather_at_place(owm_city)
